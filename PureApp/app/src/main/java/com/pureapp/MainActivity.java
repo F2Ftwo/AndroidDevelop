30ebe8e44a3ee008ab25e41df5416cf7870f0eed
Ftwo <yan.smirnov18@gmail.com>
2016-10-11 17:24:40 +0400

    Modife MainActivity - add Start screen

--- PureApp/app/src/main/java/com/pureapp/MainActivity.java
+++ PureApp/app/src/main/java/com/pureapp/MainActivity.java
@@ -1,14 +1,20 @@
 package com.pureapp;
 
+import android.content.res.Resources;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
+public class MainActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.start_screen);
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.start);
+        }
     }
-}
 
